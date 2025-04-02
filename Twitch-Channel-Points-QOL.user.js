a00c1b358ef0b9ef7cfb09d425e4c8896d52b19b
Adam <dennisaj@outlook.com>
2025-04-02 01:01:08 +0100

    Uses SVG instead of fa
    Moved to SVG instead of importing Font Awesome library

--- Twitch-Channel-Points-QOL.user.js
+++ Twitch-Channel-Points-QOL.user.js
@@ -5,22 +5,21 @@
 // @license     CC0
 // @match       https://www.twitch.tv/*
 // @icon        https://static.twitchcdn.net/assets/favicon-32-e29e246c157142c94346.png
-// @version     1.0
+// @version     1.1
 // @namespace https://github.com/LoneDestroyer
 // @downloadURL https://raw.githubusercontent.com/LoneDestroyer/Twitch-Channel-Points-QOL/main/Twitch-Channel-Points-QOL.user.js
 // @updateURL https://raw.githubusercontent.com/LoneDestroyer/Twitch-Channel-Points-QOL/main/Twitch-Channel-Points-QOL.user.js
 // ==/UserScript==
 
+/*
+ Changelog:
+ v1.1 - No longer imports font-awesome, uses SVG for the remove button
+ v1.0 - Added hiding and restoring of buttons
+ v0.5 - Initial release (renames rewards)
+*/
+
 (function() {
     'use strict';
-
-    // Load Font Awesome library
-    if (!document.querySelector('link[href*="fontawesome"]')) {
-        const link = document.createElement('link');
-        link.rel = 'stylesheet';
-        link.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css';
-        document.head.appendChild(link);
-    }
 
     // Define the specific CSS selector for the reward
     const rewardSelector = 'div.Layout-sc-1xcs6mc-0.iqUbUe.reward-list-item';
@@ -131,8 +130,8 @@
                 <div class="ScFigure-sc-wkgzod-0 fewniq tw-svg">
                     <svg width="20" height="20" viewBox="0 0 20 20" focusable="false" aria-hidden="true" role="presentation">
                         <path d="M8.5 10 4 5.5 5.5 4 10 8.5 14.5 4 16 5.5 11.5 10l4.5 4.5-1.5 1.5-4.5-4.5L5.5 16 4 14.5 8.5 10z" fill="var(--color-fill-button-icon)"></path>
-                    </svg>
-                </div>`;
+                    </svg> 
+                </div>`; // Twitch close icon
             closeButton.appendChild(closeIcon);
 
             // Add event listener to close the restore container & hover over to match Twitch style
@@ -198,7 +197,7 @@
     };
 
     // Renmae rewards based on the text content & add a "Remove" button
-    function updateRewards() {  
+    function updateRewards() {
         const rewardElements = document.querySelectorAll(rewardSelector);
         rewardElements.forEach(rewardElement => {
             const textElement = rewardElement.querySelector('div.Layout-sc-1xcs6mc-0.auOiD > p.CoreText-sc-1txzju1-0.javhvP');
@@ -208,7 +207,11 @@
                 if (!rewardElement.querySelector('.remove-reward-button')) {
                     const removeButton = document.createElement('button');
                     removeButton.className = 'remove-reward-button';
-                    removeButton.innerHTML = '<i class="fa-solid fa-eye-slash"></i>';
+                    removeButton.innerHTML = `
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" style="width: 16px; height: 16px;">
+                            <path fill="white" d="M38.8 5.1C28.4-3.1 13.3-1.2 5.1 9.2S-1.2 34.7 9.2 42.9l592 464c10.4 8.2 25.5 6.3 33.7-4.1s6.3-25.5-4.1-33.7L525.6 386.7c39.6-40.6 66.4-86.1 79.9-118.4c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C465.5 68.8 400.8 32 320 32c-68.2 0-125 26.3-169.3 60.8L38.8 5.1zM223.1 149.5C248.6 126.2 282.7 112 320 112c79.5 0 144 64.5 144 144c0 24.9-6.3 48.3-17.4 68.7L408 294.5c8.4-19.3 10.6-41.4 4.8-63.3c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3c0 10.2-2.4 19.8-6.6 28.3l-90.3-70.8zM373 389.9c-16.4 6.5-34.3 10.1-53 10.1c-79.5 0-144-64.5-144-144c0-6.9 .5-13.6 1.4-20.2L83.1 161.5C60.3 191.2 44 220.8 34.5 243.7c-3.3 7.9-3.3 16.7 0 24.6c14.9 35.7 46.2 87.7 93 131.1C174.5 443.2 239.2 480 320 480c47.8 0 89.9-12.9 126.2-32.5L373 389.9z"/>
+                        </svg>
+                    `; // Font Awesome eye-slash icon
                     removeButton.style = `
                         position: absolute; top: 5px; right: 5px; width: 20px; height: 20px;
                         background-color: rgba(0, 0, 0, 0.6); color: #fff; border-radius: 50%;
